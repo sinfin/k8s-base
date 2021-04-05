368686709f491f5e892845c16f3d208e60c442ec
Jindrich Skupa <jindrich.skupa@gmail.com>
2021-04-05 19:00:06 +0200

    Add monitogin gems and upgrade ingress

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -7,8 +7,13 @@ tar xvzf k8s-base.tar.gz && \
 	rmdir sinfin-k8s-base*
 git checkout -b dockerize
 git checkout README.md
+cat << EOF >> Gemfile
+
+gem "sidekiq-monitoring", "1.3.4"
+gem "status-page", "0.1.5"
+EOF
 git add kubernetes docker Dockerfile* prepare_test.sh docker-compose.yml .gitlab-ci.yml
-git add scripts
+git add scripts Gemfile
 git commit -m "Dockerize application"
 git push -u origin dockerize
 repo=$(git remote get-url origin | cut -d: -f2- | sed 's/.git$//')
